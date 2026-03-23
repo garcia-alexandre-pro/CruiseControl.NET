74ac49b8e4cf40dc5f2876aa761e351c1a43c0a0
garcia-alexandre-pro <garcia-alexandre-pro@users.noreply.github.com>
2026-03-23 16:36:34 +0100

    Revert "Initialization and first updates"
    This reverts commit 061377e216ecf1c618ebcc9243c46a6cf73516a5.

--- project/UnitTests/Core/SourceControl/FileSourceControlTest.cs
+++ project/UnitTests/Core/SourceControl/FileSourceControlTest.cs
@@ -10,151 +10,151 @@ using ThoughtWorks.CruiseControl.Core.Util;
 
 namespace ThoughtWorks.CruiseControl.UnitTests.Core.Sourcecontrol
 {
-    [TestFixture]
-    public class FileSourceControlTest : IntegrationFixture
-    {
-        private FileSourceControl sc;
-        private Mock<IFileSystem> fileSystemMock;
-        private SystemPath tempRoot;
-        private SystemPath tempSubRoot;
+	[TestFixture]
+	public class FileSourceControlTest : IntegrationFixture
+	{
+		private FileSourceControl sc;
+		private Mock<IFileSystem> fileSystemMock;
+		private SystemPath tempRoot;
+		private SystemPath tempSubRoot;
 
-        [SetUp]
-        public void SetUp()
-        {
-            fileSystemMock = new Mock<IFileSystem>();
+		[SetUp]
+		public void SetUp()
+		{
+			fileSystemMock = new Mock<IFileSystem>();
 
-            tempRoot = SystemPath.UniqueTempPath();
-            tempSubRoot = tempRoot.Combine("subrepo");
+			tempRoot = SystemPath.UniqueTempPath();
+			tempSubRoot = tempRoot.Combine("subrepo");
 
-            sc = new FileSourceControl((IFileSystem)fileSystemMock.Object);
-            sc.RepositoryRoot = tempRoot.ToString();
-        }
+			sc = new FileSourceControl((IFileSystem) fileSystemMock.Object);
+			sc.RepositoryRoot = tempRoot.ToString();
+		}
 
-        [TearDown]
-        public void TearDown()
-        {
-            tempRoot.DeleteDirectory();
-        }
+		[TearDown]
+		public void TearDown()
+		{
+			tempRoot.DeleteDirectory();
+		}
 
-        [Test]
-        public void MissingDirectoryThrowsException()
-        {
-            Assert.IsFalse(tempRoot.Exists(), "Temporary directory should not exist: " + tempRoot.ToString());
+		[Test]
+		public void MissingDirectoryThrowsException()
+		{
+			Assert.IsFalse(tempRoot.Exists(), "Temporary directory should not exist: " + tempRoot.ToString());
             Assert.That(delegate { sc.GetModifications(IntegrationResult(DateTime.MinValue), IntegrationResult(DateTime.MaxValue)); },
                         Throws.TypeOf<DirectoryNotFoundException>());
-        }
+		}
 
-        [Test]
-        public void IgnoreMissingDirectoryReturnsZeroMods()
-        {
-            Assert.IsFalse(tempRoot.Exists(), "Temporary directory should not exist: " + tempRoot.ToString());
-            sc.IgnoreMissingRoot = true;
-            try
-            {
-                Modification[] mods = sc.GetModifications(IntegrationResult(DateTime.MinValue), IntegrationResult(DateTime.MaxValue));
-                Assert.AreEqual(0, mods.Length, "Modifications found in a missing directory");
-            }
-            finally
-            {
-                sc.IgnoreMissingRoot = false;
-            }
-        }
+		[Test]
+		public void IgnoreMissingDirectoryReturnsZeroMods()
+		{
+			Assert.IsFalse(tempRoot.Exists(), "Temporary directory should not exist: " + tempRoot.ToString());
+			sc.IgnoreMissingRoot = true;
+			try
+			{
+				Modification[] mods = sc.GetModifications(IntegrationResult(DateTime.MinValue), IntegrationResult(DateTime.MaxValue));
+				Assert.AreEqual(0, mods.Length, "Modifications found in a missing directory");
+			}
+			finally
+			{
+				sc.IgnoreMissingRoot = false;
+			}
+		}
 
-        [Test]
-        public void GetModifications_EmptyLocal()
-        {
-            tempRoot.CreateDirectory();
-            tempSubRoot.CreateDirectory();
-            string file1 = tempRoot.CreateTextFile("file1.txt", "foo").ToString();
-            string file2 = tempRoot.CreateTextFile("file2.txt", "bar").ToString();
-            string file3 = tempSubRoot.CreateTextFile("file3.txt", "bat").ToString();
+		[Test]
+		public void GetModifications_EmptyLocal()
+		{
+			tempRoot.CreateDirectory();
+			tempSubRoot.CreateDirectory();
+			string file1 = tempRoot.CreateTextFile("file1.txt", "foo").ToString();
+			string file2 = tempRoot.CreateTextFile("file2.txt", "bar").ToString();
+			string file3 = tempSubRoot.CreateTextFile("file3.txt", "bat").ToString();
 
-            Modification[] mods = sc.GetModifications(IntegrationResult(DateTime.MinValue), IntegrationResult(DateTime.MaxValue));
+			Modification[] mods = sc.GetModifications(IntegrationResult(DateTime.MinValue), IntegrationResult(DateTime.MaxValue));
             Array.Sort(
                 mods,
-                (left, right) =>
+                (left, right) => 
                 {
                     int result = left.FolderName.CompareTo(right.FolderName);
-                    if (result == 0)
+                    if (result == 0) 
                         result = left.FileName.CompareTo(right.FileName);
-
+                        
                     return result;
                 }
             );
 
-            using (new AssertionScope())
-            {
-                mods.Length.Should().Be(4);
-                mods[0].FileName.Should().Be("file1.txt", "FileName[0]");
-                mods[1].FileName.Should().Be("file2.txt", "FileName[1]");
-                mods[2].FileName.Should().Be("file3.txt", "FileName[2]");
-                mods[3].FileName.Should().Be(Path.GetFileName(tempSubRoot.ToString()), "FileName[3]");
+			using( new AssertionScope())
+			{
+				mods.Length.Should().Be(4);
+				mods[0].FileName.Should().Be("file1.txt", "FileName[0]");
+				mods[1].FileName.Should().Be("file2.txt", "FileName[1]");
+				mods[2].FileName.Should().Be("file3.txt", "FileName[2]");
+				mods[3].FileName.Should().Be(Path.GetFileName(tempSubRoot.ToString()), "FileName[3]");
 
-                mods[0].FolderName.Should().Be(Path.GetDirectoryName(file1), "FolderName[0]");
-                mods[1].FolderName.Should().Be(Path.GetDirectoryName(file2), "FolderName[1]");
-                mods[2].FolderName.Should().Be(Path.GetDirectoryName(file3), "FolderName[2]");
-                mods[3].FolderName.Should().Be(Path.GetFileName(tempSubRoot.ToString()), "FolderName[3]");
+				mods[0].FolderName.Should().Be(Path.GetDirectoryName(file1), "FolderName[0]");
+				mods[1].FolderName.Should().Be(Path.GetDirectoryName(file2), "FolderName[1]");
+				mods[2].FolderName.Should().Be(Path.GetDirectoryName(file3), "FolderName[2]");
+				mods[3].FolderName.Should().Be(Path.GetFileName(tempSubRoot.ToString()), "FolderName[3]");
 
-                new FileInfo(file1).LastWriteTime.Should().BeCloseTo(mods[0].ModifiedTime, new TimeSpan(100), "LastWriteTime[0]");
-                new FileInfo(file2).LastWriteTime.Should().BeCloseTo(mods[1].ModifiedTime, new TimeSpan(100), "LastWriteTime[1]");
-                new FileInfo(file3).LastWriteTime.Should().BeCloseTo(mods[2].ModifiedTime, new TimeSpan(100), "LastWriteTime[2]");
-                new FileInfo(tempSubRoot.ToString()).LastWriteTime.Should().BeCloseTo(mods[3].ModifiedTime, new TimeSpan(100), "LastWriteTime[3]");
-            }
+				new FileInfo(file1).LastWriteTime.Should().BeCloseTo(mods[0].ModifiedTime,  100, "LastWriteTime[0]");
+				new FileInfo(file2).LastWriteTime.Should().BeCloseTo(mods[1].ModifiedTime, 100, "LastWriteTime[1]");
+				new FileInfo(file3).LastWriteTime.Should().BeCloseTo(mods[2].ModifiedTime, 100, "LastWriteTime[2]");
+				new FileInfo(tempSubRoot.ToString()).LastWriteTime.Should().BeCloseTo(mods[3].ModifiedTime, 100, "LastWriteTime[3]");
+			}
 
             mods = sc.GetModifications(IntegrationResult(DateTime.Now.AddHours(1)), IntegrationResult(DateTime.MaxValue));
 
-            mods.Length.Should().Be(0);
-        }
+			mods.Length.Should().Be(0);
+		}
 
-        [Test]
-        public void GetModificationsWhenRepositoryFolderIsEmpty()
-        {
-            tempRoot.CreateDirectory();
-            Modification[] mods = sc.GetModifications(IntegrationResult(DateTime.MinValue), IntegrationResult(DateTime.MaxValue));
-            Assert.IsNotNull(mods);
-            Assert.AreEqual(0, mods.Length);
-        }
+		[Test]
+		public void GetModificationsWhenRepositoryFolderIsEmpty()
+		{
+			tempRoot.CreateDirectory();
+			Modification[] mods = sc.GetModifications(IntegrationResult(DateTime.MinValue), IntegrationResult(DateTime.MaxValue));
+			Assert.IsNotNull(mods);
+			Assert.AreEqual(0, mods.Length);
+		}
 
-        [Test]
-        public void GetModificationsWhenRepositoryRootContainsOneUnmodifiedFile()
-        {
-            tempRoot.CreateDirectory();
-            tempRoot.CreateTextFile("file1.txt", "foo");
-            string file2 = tempRoot.CreateTextFile("file2.txt", "bar").ToString();
-            new FileInfo(file2).LastWriteTime = DateTime.Now.AddHours(2);
+		[Test]
+		public void GetModificationsWhenRepositoryRootContainsOneUnmodifiedFile()
+		{
+			tempRoot.CreateDirectory();
+			tempRoot.CreateTextFile("file1.txt", "foo");
+			string file2 = tempRoot.CreateTextFile("file2.txt", "bar").ToString();
+			new FileInfo(file2).LastWriteTime = DateTime.Now.AddHours(2);
 
-            Modification[] mods = sc.GetModifications(IntegrationResult(DateTime.Now.AddHours(1)), IntegrationResult(DateTime.MaxValue));
-            Assert.AreEqual(1, mods.Length);
-            Assert.AreEqual("file2.txt", mods[0].FileName);
-        }
+			Modification[] mods = sc.GetModifications(IntegrationResult(DateTime.Now.AddHours(1)), IntegrationResult(DateTime.MaxValue));
+			Assert.AreEqual(1, mods.Length);
+			Assert.AreEqual("file2.txt", mods[0].FileName);
+		}
 
-        [Test]
-        public void ShouldCopyRespositoryRootToWorkingDirectoryForGetSource()
-        {
-            IntegrationResult result = (IntegrationResult)Integration("foo", "myWorkingDirectory", "myArtifactDirectory");
-            sc.AutoGetSource = true;
-            fileSystemMock.Setup(fileSystem => fileSystem.Copy(tempRoot.ToString(), "myWorkingDirectory")).Verifiable();
+		[Test]
+		public void ShouldCopyRespositoryRootToWorkingDirectoryForGetSource()
+		{
+			IntegrationResult result = (IntegrationResult) Integration("foo", "myWorkingDirectory", "myArtifactDirectory");
+			sc.AutoGetSource = true;
+			fileSystemMock.Setup(fileSystem => fileSystem.Copy(tempRoot.ToString(), "myWorkingDirectory")).Verifiable();
 
-            sc.GetSource(result);
+			sc.GetSource(result);
 
-            fileSystemMock.Verify();
-        }
+			fileSystemMock.Verify();
+		}
 
-        [Test]
-        public void ShouldNotCopySourceIfAutoGetSourceNotBeenSetToTrue()
-        {
+		[Test]
+		public void ShouldNotCopySourceIfAutoGetSourceNotBeenSetToTrue()
+		{
             IIntegrationResult result = Integration("foo", "myWorkingDirectory", "myArtifactDirectory");
 
-            sc.GetSource(result);
+			sc.GetSource(result);
 
-            fileSystemMock.Verify();
-            fileSystemMock.VerifyNoOtherCalls();
-        }
+			fileSystemMock.Verify();
+			fileSystemMock.VerifyNoOtherCalls();
+		}
 
-        private IntegrationResult IntegrationResult(DateTime date)
-        {
-            return IntegrationResultMother.CreateSuccessful(date);
-        }
+		private IntegrationResult IntegrationResult(DateTime date)
+		{
+			return IntegrationResultMother.CreateSuccessful(date);
+		}
 
         [Test]
         public void GetModifications_NonRecursive()
@@ -173,25 +173,25 @@ namespace ThoughtWorks.CruiseControl.UnitTests.Core.Sourcecontrol
                 (left, right) => left.FileName.CompareTo(right.FileName)
             );
 
-            using (new AssertionScope())
-            {
-                mods.Length.Should().Be(3);
+			using(new AssertionScope())
+			{
+				mods.Length.Should().Be(3);
 
-                mods[0].FileName.Should().Be("file1.txt");
-                mods[1].FileName.Should().Be("file2.txt");
-                mods[2].FileName.Should().Be(Path.GetFileName(tempSubRoot.ToString()));
+				mods[0].FileName.Should().Be("file1.txt");
+				mods[1].FileName.Should().Be("file2.txt");
+				mods[2].FileName.Should().Be(Path.GetFileName(tempSubRoot.ToString()));
 
-                mods[0].FolderName.Should().Be(Path.GetDirectoryName(file1));
-                mods[1].FolderName.Should().Be(Path.GetDirectoryName(file2));
-                mods[2].FolderName.Should().Be(Path.GetFileName(tempSubRoot.ToString()));
+				mods[0].FolderName.Should().Be(Path.GetDirectoryName(file1));
+				mods[1].FolderName.Should().Be(Path.GetDirectoryName(file2));
+				mods[2].FolderName.Should().Be(Path.GetFileName(tempSubRoot.ToString()));
 
-                new FileInfo(file1).LastWriteTime.Should().BeCloseTo(mods[0].ModifiedTime, new TimeSpan(100));
-                new FileInfo(file2).LastWriteTime.Should().BeCloseTo(mods[1].ModifiedTime, new TimeSpan(100));
-                new FileInfo(tempSubRoot.ToString()).LastWriteTime.Should().BeCloseTo(mods[2].ModifiedTime, new TimeSpan(100));
-            }
+				new FileInfo(file1).LastWriteTime.Should().BeCloseTo(mods[0].ModifiedTime, 100);
+				new FileInfo(file2).LastWriteTime.Should().BeCloseTo(mods[1].ModifiedTime, 100);
+				new FileInfo(tempSubRoot.ToString()).LastWriteTime.Should().BeCloseTo(mods[2].ModifiedTime, 100);
+			}
 
             mods = sc.GetModifications(IntegrationResult(DateTime.Now.AddHours(1)), IntegrationResult(DateTime.MaxValue));
-            mods.Length.Should().Be(0);
+			mods.Length.Should().Be(0);
         }
-    }
+	}
 }
